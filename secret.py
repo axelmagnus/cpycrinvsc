a16c2f840b25d42de9ff290256a201d6a169cd51
Axel Månsson <axel.magnus.mansson@gmail.com>
2022-02-08 13:52:46 +0100

    secrets added

---
+++ secret.py
@@ -0,0 +1,16 @@
+secrets = {
+    # 2.4 MegaPlupp Vinnys Bar Guest Axels Moto Keep the two '' quotes around the name
+    'ssid': '2.4 MegaPlupp',
+    'password': 'azazaz13',  # 'Vinnys.Bar101' Keep the two '' quotes around password
+    'timezone': "Europe/Stockholm",  # http://worldtimeapi.org/timezones
+    'aio_username': 'axelmagnus',
+    'aio_key': '1a20315d078d4304bee799ce4b2af0e7',
+    'aio_port': '8883',
+    'openweather_token': 'b7f3173670086bfc757efcaf35185060',
+    'city_id': '2692965',
+    'celcius': True,
+    'google_client_id': '454745915011-pkuehk32nn5t82kde4cnvqfcucihef0s.apps.googleusercontent.com',
+    'google_client_secret': 'fnXtH4fVj0N7EBtuXCIu9ykY',
+   	'google_access_token': 'ya29.a0AfH6SMDEYizRew7zHTOc7wg5Ew076WOv05wenDtXvNqvVqrjGLMWGMP9SQjbxLM2205GPSRPy3Ff09dtkjMejiMqips14ql9wZmORtVBTpDNxp1wcQlhPVK9ChspoRyNssp-qUwj27voQI8b1kKftzD5xUgga4pBueMxssd7jTM',
+   	'google_refresh_token': '1//0c-Nsju4bwg8LCgYIARAAGAwSNwF-L9IrWVqR-a4zgLm0qmSt9AX_CsuRzzgqxfH6-FGTtACxpvHpE_zi0S2RXJ7m6bzKIQomSec'
+}
